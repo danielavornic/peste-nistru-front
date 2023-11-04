76e4f26b2d7121d85f1a79ebf06338b0fecbee6a
Daniela Vornic <daniela.vornic@gmail.com>
2023-11-04 16:47:54 +0200

    Init with chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+
+const theme = extendTheme({
+  colors: {
+    white: "#FFFFFF",
+    black: "#1A1A1A",
+    brand: {
+      100: "rgb(14, 176, 133, 0.1)",
+      400: "#0b8e6b",
+      500: "#0EB085",
+      600: "#0b8e6b",
+    },
+    accent: {
+      200: "#fcdfcf",
+      500: "#F7A072",
+    },
+  },
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    body: `'Open Sans', sans-serif`,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
